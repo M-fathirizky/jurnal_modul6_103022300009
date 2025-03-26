527c7aae33b23db337071cc2ebcc7eca492ee27a
M-fathirizky <fathirrizky636@gmail.com>
2025-03-26 09:18:59 +0800

    penambahan design by contract pada semua kelas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul6_103022300009
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SayaTubeUser user = new SayaTubeUser("Muhammad Fathir Rizky Salam");
+
+                for (int i = 1; i <= 8; i++)
+                {
+                    string videoTitle = $"Review Film Bagus untuk di tonton{i}";
+
+                    SayaTubeVideo video = new SayaTubeVideo(videoTitle);
+
+                    video.IncreasePlayCount(1000000 * i);
+
+                    user.addVideo(video);
+                }
+                Console.WriteLine("Total play count:{user.GetTotalVideoPlayCount()}");
+
+                user.PrintAllVideoPlayCount();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Terjadi Kesalahan: {e.Message}");
+            }
+        }
+    }
+}
