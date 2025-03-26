cf4df7271f74de9b8504e8f7ded566cfd8d1b634
M-fathirizky <fathirrizky636@gmail.com>
2025-03-26 08:57:53 +0800

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ SayaTubeUser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300009
+{
+    class SayaTubeUser
+    {
+        private int id;
+        private string username;
+        private List<SayaTubeVideo> uploadedVideos;
+
+        public SayaTubeUser(string username)
+        {
+            if (string.IsNullOrEmpty(username) || username.Length > 100)
+            {
+                throw new ArgumentException("Username tidak boleh kosong dan tidak boleh lebih dari 100 karakter");
+            }
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.username = username;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+        public int GetTotalVideoPlayCount()
+        {
+            int totalPlayCount = 0;
+            foreach (SayaTubeVideo video in uploadedVideos)
+            {
+                totalPlayCount += video.GetPlayCount();
+            }
+            return totalPlayCount;
+        }
+
+        public void addVideo(SayaTubeVideo video)
+        {
+            if (video == null)
+            {
+                throw new ArgumentNullException("Video tidak boleh null");
+            }
+
+            if (uploadedVideos.Count >= 8)
+            {
+                throw new Exception("Video sudah mencapai batas maksimal");
+            }
+            uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine("User: " + username);
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                Console.WriteLine("Video " + (i + 1) + " Judul: " + uploadedVideos[i].GetTitle());
+            }
+
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300009
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Video title cannot be empty");
+            }
+
+            if (title.Length > 100)
+            {
+                throw new ArgumentException("Video title cannot be more than 100 characters");
+            }
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);  
+            this.title = title;
+            this.playCount = 0;
+
+            Debug.Assert(this.playCount == 0, "Error Playcount harus 0 saat objek di buat!");
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 25000000)
+            {
+                throw new ArgumentOutOfRangeException("Invalid play count increment");
+            }
+
+            Debug.Assert(playCount + count >= playCount, "Error : terjadi overflow pada playCount!");
+
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                this.playCount = int.MaxValue;
+
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID : {id}");
+            Console.WriteLine($"Title: {title}" );
+            Console.WriteLine("Play Count: {playCount}");
+        }
+
+        public int GetPlayCount() => playCount;
+        public string GetTitle() =>  title;
+    }
+}
